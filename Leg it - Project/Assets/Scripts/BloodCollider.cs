d8b3ea2acd6c5cb03285d41afca9573d45ecabc4
Leyla Asan Mane <lasan001@gold.ac.uk>
2018-02-24 18:04:12 +0000

    tried to add blood but it's not working because of scripting

---
+++ Leg it - Project/Assets/Scripts/BloodCollider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodCollider : MonoBehaviour {
+    public GameObject blood;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag.Equals("Damager"))
+        {
+            Instantiate(blood, transform.position, Quaternion.identity);
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+
+    }
+}
--- Leg it - Project/Assets/Scripts/Health.cs
+++ Leg it - Project/Assets/Scripts/Health.cs
@@ -14,6 +14,10 @@ public class Health : MonoBehaviour {
     float maxHealth = 100f;
     float CurrentHealth;
 
+    private movement player;//allow to use the variables from the movement class.
+    public GameObject BloodParticle;
+    public GameObject RespawnParticle; //This will be moved once I have a respawn location
+
 
     void Start () {
 
@@ -26,13 +30,27 @@ public class Health : MonoBehaviour {
         healthValue.text = CurrentHealth.ToString() + "%";
 	}
 
+    
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "damager")
         {
-
+            
             HealthBar.value -= 1f;
             CurrentHealth = HealthBar.value;
         }
     }
+
+    public void Death()
+    {
+        if (CurrentHealth == 0)
+        {
+
+            Instantiate(BloodParticle, player.transform.position, player.transform.rotation);//make a copy of bloodparticle in this position and rotation, of the player's. 
+            player.transform.position = new Vector2(50, 0);
+        }
+
+    }
+
 }
